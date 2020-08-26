466dca1df091c5b4f72020c979f090df174d2a25
siting <zhaositing1026@gmail.com>
2020-08-26 14:52:53 +0800

    added get average script

---
+++ scripts/get_average.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+path='../temp/ann/log/gui/test/accuracy.txt'
+
+data = pd.read_csv(path, sep=" ", header=None, skiprows=[0])
+
+data.columns = ["index", "snn top 1", "top 1", "ann top 1", "ann"]
+
+data['snn top 1'] = data['snn top 1'].str[:-1].astype(float)
+data['top 1'] = data['top 1'].str[:-1].astype(float)
+data['ann top 1'] = data['ann top 1'].str[:-1].astype(float)
+data['ann'] = data['ann'].str[:-1].astype(float)
+
+sum = data.sum(axis = 0, skipna = True)
+result = sum/30
+
+print(result)
